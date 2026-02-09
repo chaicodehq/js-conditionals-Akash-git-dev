c47e9ac21396d33f08bdf61fa6cc3446d910536a
Akash Dey <akashdey0802@gmail.com>
2026-02-09 20:07:39 +0530

    Completed 06-shipping-calculator

--- src/06-shipping-calculator.js
+++ src/06-shipping-calculator.js
@@ -30,4 +30,44 @@
  */
 export function calculateShipping(weight, country, orderTotal) {
   // Your code here
+
+   if(weight<=0 || orderTotal<0) return -1;
+
+   let shippingCost;
+
+   if(country==="US")
+   {
+        if(orderTotal>50) 
+          { shippingCost=0;
+          }
+
+        else if(weight<=1) 
+          { shippingCost=5;
+          }
+
+        else if(weight<=5)
+        {
+           shippingCost=10;
+        }
+        else{
+          shippingCost=15;
+        }
+   }
+   else{
+       if (orderTotal > 100) {
+         shippingCost = 0;
+       } else if (weight <= 1) {
+         shippingCost = 15;
+       } else if (weight <= 5) {
+         shippingCost = 25;
+       } else {
+         shippingCost = 40;
+       }
+      
+
+   }
+
+   return shippingCost;
+
+
 }
